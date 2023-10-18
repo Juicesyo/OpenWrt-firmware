e63a3911059ae8065401061c936309efcd613092
Juice <315294716@qq.com>
2023-10-18 12:29:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-#git clone https://github.com/Zxilly/UA2F package/UA2F
+sudo chmod -R 777 openwrt
 
+#git clone https://github.com/Zxilly/UA2F package/UA2F
 #git clone https://github.com/Juicesyo/OpenClash package/luci-app-openclash
